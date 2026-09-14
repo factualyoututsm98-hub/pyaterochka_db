81da493b65b50b0273f93e5370e1881d01ccb797
Noize218 <factualyoututsm98@gmail.com>
2026-09-14 14:18:28 +0300

    Update full dump

--- pyaterochka.sql
+++ pyaterochka.sql
@@ -1,1 +1,16 @@
-SHOW TABLES;
+-- Pyaterochka full dump
+-- Schema + Data + Queries
+
+CREATE DATABASE IF NOT EXISTS pyaterochka;
+USE pyaterochka;
+
+-- Schema
+CREATE TABLE categories (...);
+...
+
+-- Data
+INSERT INTO categories (...) VALUES (...);
+...
+
+-- Queries
+SELECT ...;
